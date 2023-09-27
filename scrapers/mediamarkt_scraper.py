c736160dcf99958e56dc3af8de0e8073f3dedc0a
ChristosRaptis <chrisgraptis@gmail.com>
2023-09-27 13:26:07 +0200

    progress

--- scrapers/mediamarkt_scraper.py
+++ scrapers/mediamarkt_scraper.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import json
 from bs4 import BeautifulSoup as bs
 from playwright.async_api import async_playwright
@@ -127,6 +128,38 @@ async def main():
     elapsed_time = end_time - start_time
     print(f"Scraping completed in {elapsed_time/60:.2f} minutes.")
 
+=======
+import asyncio
+from playwright.async_api import async_playwright
+from bs4 import BeautifulSoup as bs
+
+multimedia_url = (
+    "https://www.mediamarkt.be/fr/category/_ordinateur-multim%C3%A9dia-452669.html"
+)
+
+ul_selector = "#floating-ui-5 > ul"
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        consent_button = page.locator("#pwa-consent-layer-accept-all-button")
+        categories_button = page.locator("#main-content > div.sc-dbb4f5b6-0.dMPtoe >\
+                                        div.sc-490debc5-0.jhMMyN > div.sc-835d3f28-0.kTdeVZ > aside > div:nth-child(1) > aside > div > button") 
+        await page.goto(multimedia_url) 
+        await consent_button.click()
+        print("Consent button clicked")
+        await categories_button.click()
+        print("categories button clicked")
+        html = await page.content()
+        soup = bs(html, "html.parser")
+        categories
+        categories = soup.find_all("a", attrs={"class": "sc-db43135e-0 bjnEhE"})
+        print(type(categories))
+        links = [a["href"] for a in categories]
+        print(links)
+        print(len(links))
+        await browser.close()
+>>>>>>> 437a1b2 (progress)
 
 
 if __name__ == "__main__":
