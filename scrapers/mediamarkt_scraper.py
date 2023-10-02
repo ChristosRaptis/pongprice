018c72897a3fb843acc7f1f570857cacea4dc443
ChristosRaptis <chrisgraptis@gmail.com>
2023-10-02 17:13:28 +0200

    connected to postgresql database

--- scrapers/mediamarkt_scraper.py
+++ scrapers/mediamarkt_scraper.py
@@ -1,4 +1,3 @@
-
 import json
 from bs4 import BeautifulSoup as bs
 import httpx
@@ -17,40 +16,45 @@ def get_data(url: str) -> list:
         while page_count <= max_pages:
             print("-" * 100)
             print(f"Scraping page {page_count + 1}: {current_url}")
-            response =  httpx.get(current_url, timeout=90.0)
+            response = httpx.get(current_url, timeout=90.0)
             soup = bs(response.content, "html.parser")
             divs = soup.find_all(
-                "div", attrs={"class": "sc-57bbc469-0 hhSaVb sc-5bb8ec6d-3 YFARY sc-b0d9c874-1 bRUDYz"}
+                "div",
+                attrs={
+                    "class": "sc-57bbc469-0 hhSaVb sc-5bb8ec6d-3 YFARY sc-b0d9c874-1 bRUDYz"
+                },
             )
             for div in divs:
                 product = {}
-                a_tag = div.find("a", attrs={"class": "sc-db43135e-1 gpEOUZ sc-b0d9c874-0 gJSJVL"})
+                a_tag = div.find(
+                    "a", attrs={"class": "sc-db43135e-1 gpEOUZ sc-b0d9c874-0 gJSJVL"}
+                )
                 try:
                     product["product_url"] = base_url + a_tag["href"]
                 except:
-                    product["product_url"] = "url not available"
-                print(product["product_url"])    
+                    product["product_url"] = current_url
+                print(product["product_url"])
                 try:
                     product["product_name"] = a_tag.text
-                except AttributeError :
-                        try:
-                            product["product_name"] = a_tag.find("p").text    
-                        except AttributeError :
-                            product["product_name"] = "name not available"
+                except AttributeError:
+                    try:
+                        product["product_name"] = a_tag.find("p").text
+                    except AttributeError:
+                        product["product_name"] = "name not available"
                 price_div = div.find("div", attrs={"class": "sc-3bd4ad78-0 kQSbne"})
                 try:
                     product["product_price"] = price_div.find("span").text.strip()
-                except AttributeError :
-                    product["product_price"] = "price not available"    
+                except AttributeError:
+                    product["product_price"] = "price not available"
                 product_list.append(product)
-            if not soup.find("button", class_="sc-21f2092b-1 eTQftF sc-2469269c-1 eeiKDF"):
+            if not soup.find(
+                "button", class_="sc-21f2092b-1 eTQftF sc-2469269c-1 eeiKDF"
+            ):
                 break
             page_count += 1
             current_url = url + f"?page={page_count + 1}"
             pbar.update(1)
     return product_list
-
-
 
 
 def main():
@@ -65,10 +69,10 @@ def main():
         print(f"{len(final_list)} products scraped.")
         print("-" * 100)
     with open("products.json", "w") as f:
-        json.dump(final_list, f, indent=4)    
+        json.dump(final_list, f, indent=4)
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
-    print(f"Scraping completed in {elapsed_time:.2f} seconds.")
+    print(f"Scraping completed in {elapsed_time/60:.2f} minutes.")
 
 
 if __name__ == "__main__":
