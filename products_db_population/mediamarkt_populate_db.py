0c1339645258a07cd6b6bcac4f5b2b94a65154a8
ChristosRaptis <chrisgraptis@gmail.com>
2023-10-09 15:22:50 +0200

    modified files for prices

--- products_db_population/mediamarkt_populate_db.py
+++ products_db_population/mediamarkt_populate_db.py
@@ -1,5 +1,11 @@
 import psycopg2
 import json
+import re
+
+def clean_price(s):
+    s = s.replace(' ', '').replace('€', '').replace('\u202f', '').replace('–', '').replace(',', '.').rstrip('0').rstrip(',').rstrip('-')
+    return float(s)    
+
 
 conn = psycopg2.connect(
     host="localhost", dbname="postgres", user="postgres", password="1234", port=5432
@@ -11,12 +17,13 @@ with open("data/mediamarkt_products.json", "r") as f:
     data = json.load(f)
 
 cur.execute(
-    "CREATE TABLE products (url VARCHAR(255), product_name TEXT, product_price VARCHAR(255));"
+    "CREATE TABLE products (url TEXT, product_name TEXT, product_price_in_euros REAL);"
 )
 
 for item in data:
+    item["product_price"] = clean_price(item["product_price"])
     cur.execute(
-        "INSERT INTO products (url, product_name, product_price) VALUES (%s, %s, %s);",
+        "INSERT INTO products (url, product_name, product_price_in_euros) VALUES (%s, %s, %s);",
         (item["product_url"], item["product_name"], item["product_price"]),
     )
 
