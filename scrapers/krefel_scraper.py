023cdd3dfceadf568f89a639130b2a38cf5328eb
ChristosRaptis <chrisgraptis@gmail.com>
2023-10-13 16:23:36 +0200

    created scraper_utils module and tested in krefel scraper

--- scrapers/krefel_scraper.py
+++ scrapers/krefel_scraper.py
@@ -1,81 +1,81 @@
 import json
-import requests
-from bs4 import BeautifulSoup as bs
-import fake_useragent
 from concurrent.futures import ThreadPoolExecutor
 import time
 from tqdm import tqdm
+from scraper_utils import (
+    get_data_soup,
+    get_product_urls,
+    clean_price,
+    get_db_connection,
+    update_database
+    )
 
-
-def get_headers():
-    user_agent = fake_useragent.UserAgent().random
-    headers = {"User-Agent": user_agent}
-    return headers
 
 product_sitemaps = [
     f"https://media.krefel.be/sys-master/sitemap/product-fr-{n}.xml"
     for n in range(0, 20)
 ]
 
-def get_product_urls(sitemap_url):
-    headers= get_headers()
-    response = requests.get(sitemap_url, headers=headers)
-    soup = bs(response.text, "xml")
-    urls = [url.text for url in soup.find_all("loc")]
-    return urls
 
-def get_product_data(product_url):
+def get_product_data(product_url: str):
+    """
+    Returns a dictionary containing the product name and price from a product url
+
+    Args:
+        product_url (str): product url
+    Returns:
+        dict: dictionary containing the product url, name and price
+    
+    """
     print(f"Scraping {product_url}")
-    headers = get_headers()
-    try:
-        response = requests.get(product_url, headers=headers, timeout=60)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred: {e}")
-        print(f"Retrying {product_url}")
-        return {}
-
     product_data = {}
-    try:
-        soup = bs(response.text, "html.parser")
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        print(f"Retrying {product_url}")
-        return {}
 
+    soup = get_data_soup(product_url)
     product_data["url"] = product_url
     script = soup.find("script", type="application/json")
-
     dict = json.loads(script.string)
     try:
-        data = dict["props"]["pageProps"]["dehydratedState"]["queries"][0][
-            "state"
-        ]["data"]
+        data = dict["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"][
+            "data"
+        ]
         product_data["product_name"] = data["manufacturer"] + " " + data["name"]
-        product_data["product_price"] = data["price"]["formattedValue"]
+        product_data["product_price"] = clean_price(data["price"]["formattedValue"])
     except:
         print("error getting data")
-        product_data["product_name"] = "N/A"
-        product_data["product_price"] = "N/A"
-
+        product_data["product_name"] = None
+        product_data["product_price"] = None
+    # still need to test this part, maybe combine these two functions into one    
+    conn = get_db_connection()
+    cur = conn.cursor()    
+    update_database(product_data, cur)
+    conn.commit()
+    cur.close()
+    conn.close()
     return product_data
 
+
 def main():
+    
     start_time = time.perf_counter()
-    product_urls = [get_product_urls(sitemap) for sitemap in product_sitemaps]
-    product_urls = [
-        url for sublist in product_urls for url in sublist
-    ]
+    product_urls = get_product_urls(product_sitemaps)
+    print(f"Found {len(product_urls)} product urls")
+    
     with ThreadPoolExecutor(max_workers=3) as executor:
-        product_list = list(tqdm(executor.map(get_product_data, product_urls[:10]), total=len(product_urls), desc="Scraping Krefel"))
+        product_list = list(
+            tqdm(
+                
+                executor.map(get_product_data, product_urls[:10]),
+                total=len(product_urls),
+                desc="Scraping Krefel",
+            )
+        )
     # with open("data/krefel_product_data.json", "w") as outfile:
     #     json.dump(product_list, outfile, indent=4)
-    print(f"Scraped {len(product_list)} products ")   
+    print(product_list)
+    print(f"Scraped {len(product_list)} products ")
     end_time = time.perf_counter()
     print(f"Finished in {round((end_time - start_time)/60, 2)} minutes")
 
+
 if __name__ == "__main__":
-   main()
-
-
-    
+    main()
---
+++ scrapers/scraper_utils.py
@@ -0,0 +1,140 @@
+"""
+This module contains utility functions for scrapers
+
+functions:
+- `get_product_urls`
+- `get_data_soup`
+- `clean_price`
+- `get_db_connection`
+
+libraries:
+- `requests`
+- `fake_useragent`
+- `bs4`
+- `tqdm`
+- `dotenv`
+- `os`
+- `psycopg2`
+"""
+
+import requests
+import fake_useragent
+from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+
+def get_product_urls(sitemap_urls_list: list) -> list:
+    """Returns a list of product urls from a list of sitemap urls
+
+    Args:
+        sitemap_urls_list (list): list of sitemap urls
+    Returns:
+        list: list of product urls
+    """
+
+    product_urls = []
+    for sitemap_url in tqdm(
+        sitemap_urls_list, desc="Getting product urls", total=len(sitemap_urls_list)
+    ):
+        user_agent = fake_useragent.UserAgent().random
+        headers = {"User-Agent": user_agent}
+        response = requests.get(sitemap_url, headers=headers)
+        soup = bs(response.text, "xml")
+        urls = [url.text for url in soup.find_all("loc")]
+        product_urls.extend(urls)
+    return product_urls
+
+
+def get_data_soup(product_url: str) -> bs:
+    """Returns a BeautifulSoup object from a product url
+
+    Args:
+        product_url (str): product url
+    Returns:
+        bs: BeautifulSoup object
+    """
+
+    user_agent = fake_useragent.UserAgent().random
+    headers = {"User-Agent": user_agent}
+    response = requests.get(product_url, headers=headers)
+    soup = bs(response.text, "html.parser")
+    return soup
+
+
+def clean_price(number: str) -> float:
+    """Returns a float from a string containing a price
+
+    Args:
+        number (str): string containing a price
+    Returns:
+        float: price as a float
+
+    """
+    number = (
+        number.replace(" ", "")
+        .replace("\xa0", "")
+        .replace(".", "")
+        .replace("€", "")
+        .replace("\u202f", "")
+        .replace("–", "")
+        .replace(",", ".")
+        .rstrip("0")
+        .rstrip(",")
+        .rstrip("-")
+    )
+    return float(number)
+
+
+def get_db_connection():
+    """Returns a connection to the postgreSQL database
+
+    Returns:
+        psycopg2.connection: connection to the database
+    """
+    load_dotenv()
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+
+    conn = psycopg2.connect(
+        host=db_host, port=db_port, dbname=db_name, user=db_user, password=db_password
+    )
+    return conn
+
+
+def update_database(product_data: dict, cursor) -> None:
+    """Checks the database for the product url (unique key) of the product data, if it exists it updates the price, 
+       if not it inserts the product data
+
+    Args:
+        product_data (dict): dictionary containing the product url, name and price
+        cursor: cursor to the database
+    """
+    cursor.execute(
+        "SELECT EXISTS(SELECT 1 FROM products WHERE url=%s);", (product_data["url"],)
+    )
+    exists = cursor.fetchone()[0]
+    if exists:
+        print("the product already exists, updating the price ...")
+        cursor.execute(
+            "UPDATE products SET product_price = %s WHERE url = %s;",
+            (product_data["product_price"], product_data["url"]),
+        )
+    else:
+        print("the product does not exist, inserting the product data ...")
+        cursor.execute(
+            "INSERT INTO products (url, product_name, product_price) VALUES (%s, %s, %s);",
+            (
+                product_data["url"],
+                product_data["product_name"],
+                product_data["product_price"],
+            ),
+        )
+
+
+    
