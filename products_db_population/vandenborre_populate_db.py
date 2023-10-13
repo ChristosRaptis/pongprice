c586edf3facf8a5781756efdef73eb088ae9fd00
ChristosRaptis <chrisgraptis@gmail.com>
2023-10-13 13:59:16 +0200

    change2

--- products_db_population/vandenborre_populate_db.py
+++ products_db_population/vandenborre_populate_db.py
@@ -1,13 +1,14 @@
 import psycopg2
 import json
 import pandas as pd
-import re 
+import re
 from dotenv import load_dotenv
 import os
 
 
 # Define a regular expression pattern to match storage capacity values
 pattern = r"(\d+)(G[Bo]|T[Bo])(?:/(\d+)(G[Bo]|T[Bo]))?"
+
 
 # Define a function to replace storage capacity values with the desired format
 def replace_capacity(match):
@@ -16,9 +17,22 @@ def replace_capacity(match):
     else:
         return f"{match.group(1)} {match.group(2)}"
 
+
 def clean_price(s):
-    s = s.replace(' ', '').replace('\xa0', '').replace('.', '').replace('€', '').replace('\u202f', '').replace('–', '').replace(',', '.').rstrip('0').rstrip(',').rstrip('-')
-    return float(s)    
+    s = (
+        s.replace(" ", "")
+        .replace("\xa0", "")
+        .replace(".", "")
+        .replace("€", "")
+        .replace("\u202f", "")
+        .replace("–", "")
+        .replace(",", ".")
+        .rstrip("0")
+        .rstrip(",")
+        .rstrip("-")
+    )
+    return float(s)
+
 
 load_dotenv()
 
@@ -35,13 +49,14 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 # open the csv file data/df_products_vandeborre.csv and store it in a variable called data
 data = pd.read_csv("data/df_products_vandeborre.csv", index_col=0)
-data["product_name"] = data["product_name"].apply(lambda x: re.sub(pattern, replace_capacity, x))
+data["product_name"] = data["product_name"].apply(
+    lambda x: re.sub(pattern, replace_capacity, x)
+)
 data["product_price"] = data["product_price"].apply(lambda x: clean_price(x))
 
 data = data.to_dict("records")
 
 for item in data:
-        
     cur.execute(
         "INSERT INTO products (url, product_name, product_price) VALUES (%s, %s, %s);",
         (item.get("url"), item.get("product_name"), item.get("product_price")),
