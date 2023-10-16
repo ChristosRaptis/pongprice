56b6eccc13a03294e249eb5796028d9906cdbb2e
ChristosRaptis <chrisgraptis@gmail.com>
2023-10-16 10:50:26 +0200

    last mofidication to krefel scraper all Ok

--- scrapers/krefel_scraper.py
+++ scrapers/krefel_scraper.py
@@ -7,16 +7,14 @@ from scraper_utils import (
     get_product_urls_from_xml,
     clean_price,
     get_db_connection,
-    update_database
-    )
+    update_database,
+)
 
 main_sitemap_url = "https://media.krefel.be/sys-master/sitemap/index-fr.xml"
 main_sitemap_soup = get_soup(main_sitemap_url, "xml")
 # find sitemaps that contain 'product' in the url
-product_sitemaps = [    
-    url.text
-    for url in main_sitemap_soup.find_all("loc")
-    if "product" in url.text
+product_sitemaps = [
+    url.text for url in main_sitemap_soup.find_all("loc") if "product" in url.text
 ]
 
 
@@ -28,12 +26,10 @@ def get_product_data(product_url: str):
         product_url (str): product url
     Returns:
         dict: dictionary containing the product url, name and price
-    
-    """
-    print(f"Scraping {product_url}")
-    product_data = {}
 
-    soup = get_soup(product_url, 'html.parser')
+    """
+    product_data = {}
+    soup = get_soup(product_url, "html.parser")
     product_data["url"] = product_url
     script = soup.find("script", type="application/json")
     dict = json.loads(script.string)
@@ -47,26 +43,25 @@ def get_product_data(product_url: str):
         print("error getting data")
         product_data["product_name"] = None
         product_data["product_price"] = None
-    # still need to test this part, maybe combine these two functions into one    
+    
     conn = get_db_connection()
-    cur = conn.cursor()    
+    cur = conn.cursor()
     update_database(product_data, cur)
     conn.commit()
     cur.close()
     conn.close()
+
     return product_data
 
 
 def main():
-    
     start_time = time.perf_counter()
     product_urls = get_product_urls_from_xml(product_sitemaps)
     print(f"Found {len(product_urls)} product urls")
-    
+
     with ThreadPoolExecutor(max_workers=3) as executor:
         product_list = list(
             tqdm(
-                
                 executor.map(get_product_data, product_urls),
                 total=len(product_urls),
                 desc="Scraping Krefel",
